4b59f57ba80b111b2e25fe100a3c1729f6d4721a
Davidshi1998 <shig@rose-hulman.edu>
2021-10-10 21:58:16 -0400

    Update Twitter_Module.py

--- 601 project 2/Twitter_Module.py
+++ 601 project 2/Twitter_Module.py
@@ -5,10 +5,10 @@ from IPython.display import display
 import pandas
 from social_media_module import Social_Media_Module
 
-consumer_key = "Y2R2YyiGiXa8wD7KqvLOEht5C"
-consumer_secret = "bmVpIHMybwu4eYHnasQ6kmJ7Zv5DRYO3VCUo9A82CBegR8gCc5"
-access_key = "2824609167-xdYnMeHX4Tk5kvHZOUakiP4IRKZkePP10OwP5wj"
-access_secret = "7ect2FUAHahrMPh44B2BnqLjFRJXAkHrf7oOtUq1D77wG"
+consumer_key = ""
+consumer_secret = ""
+access_key = ""
+access_secret = ""
 
 callback_uri = 'oob'
 
